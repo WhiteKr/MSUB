ea665ab3bd3f92fd31717281b6f5725d055e69de
WhiteKr <hanjisu2215g@gmail.com>
2021-04-10 22:50:11 +0900

    Added css files into folder styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
-				
+
 			</div>
 		)
 	}
