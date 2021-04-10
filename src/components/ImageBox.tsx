f485b4c7a574b36a3c78c942220c8c1e2d9b043a
WhiteKr <hanjisu2215g@gmail.com>
2021-04-11 02:41:55 +0900

    Created image backgrounded content

---
+++ src/components/ImageBox.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/ImageBox.css';
+
+class ImageBox extends React.Component {
+	render() {
+		return (
+			<div className="ImageBox">
+				<a id="forNewTry">새로운 도전을 위하여,</a>
+				<a id="MSUB">MSUB</a>
+			</div>
+		)
+	}
+}
+
+export default ImageBox;
