a26dd3f29adcc3d3cdd17f53567af89ff16b353e
WhiteKr <hanjisu2215g@gmail.com>
2021-04-11 03:16:33 +0900

    Created component EndArea

---
+++ src/components/EndArea.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/EndArea.css';
+
+class EndArea extends React.Component {
+	render() {
+		return (
+			<div className="EndArea">
+				<div id="building" />
+			</div>
+		)
+	}
+}
+
+export default EndArea;
