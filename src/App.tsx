2c0a8d6e44b9f13901f5d69dd60e72ad8b3f6521
WhiteKr <hanjisu2215g@gmail.com>
2021-04-10 23:09:04 +0900

    Added Header tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles/App.css';
 
+import Header from './components/Header';
+
 class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
-				<h3>MSUB</h3>
+				<Header />
 			</div>
 		)
 	}
