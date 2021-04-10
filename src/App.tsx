1e1fbcd4e1d8755218d50d724d700b0d60478112
WhiteKr <hanjisu2215g@gmail.com>
2021-04-11 02:42:22 +0900

    Added content to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles/App.css';
 
 import Header from './components/Header';
+import ImageBox from './components/ImageBox';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
 				<Header />
+				<ImageBox />
 			</div>
 		)
 	}
