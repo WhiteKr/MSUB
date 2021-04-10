5985310bb99625a5fcce0ce632e688a7ebc08aac
WhiteKr <hanjisu2215g@gmail.com>
2021-04-10 23:24:26 +0900

    Created Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../styles/Header.css';
 
 class Header extends React.Component {
 	render() {
 		return (
 			<div className="Header">
-
+				<a id="headerTitle">MSUB</a>
 			</div>
 		)
 	}
